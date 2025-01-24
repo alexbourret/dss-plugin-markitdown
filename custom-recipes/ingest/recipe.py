83e4c9779d6360624f79e5ca7162585dff0278fe
admin <admin@localhost>
2025-01-24 18:50:51 +0100

    Edited files

--- custom-recipes/ingest/recipe.py
+++ custom-recipes/ingest/recipe.py
@@ -37,6 +37,7 @@ output_A_datasets = [dataiku.Dataset(name) for name in output_A_names]
 # the Settings tab of the recipe. The field "params" holds a list of all the params for wich the
 # user will be prompted for values.
 
+
 # For optional parameters, you should provide a default value in case the parameter is not present:
 my_variable = get_recipe_config().get('parameter_name', None)
 
